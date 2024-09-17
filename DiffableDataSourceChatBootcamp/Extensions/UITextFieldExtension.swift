62e07c8dfaafaa57d536d41188f601fc058361ad
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-17 10:34:00 +0900

    remove: UITextFieldExtensionの使用していないコードの削除

--- DiffableDataSourceChatBootcamp/Extensions/UITextFieldExtension.swift
+++ DiffableDataSourceChatBootcamp/Extensions/UITextFieldExtension.swift
@@ -2,13 +2,6 @@
 import UIKit
 
 extension UITextField {
-    
-    func setLeftPadding() {
-        let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
-        leftView = leftPadding
-        leftViewMode = .always
-    }
-    
     func setToolbar(action: Selector?) {
         let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 45))
         let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
